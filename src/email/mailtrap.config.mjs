4ec4f898bb9a962ff50e292fff075d117d719e9f
Abu Al Nasir Mitul <nasirmitul28@gmail.com>
2024-10-04 20:55:56 +0600

    sending test email with mailtrap is successful

---
+++ src/email/mailtrap.config.mjs
@@ -0,0 +1,32 @@
+import { MailtrapClient } from "mailtrap";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const TOKEN = process.env.MAILTRAP_TOKEN;
+const ENDPOINT = process.env.MAILTRAP_ENDPOINT;
+
+const client = new MailtrapClient({
+  endpoint: ENDPOINT,
+  token: TOKEN,
+});
+
+const sender = {
+  email: "hello@demomailtrap.com",
+  name: "Nasir Mitul",
+};
+const recipients = [
+  {
+    email: "nasirmitul28@gmail.com",
+  },
+];
+
+client
+  .send({
+    from: sender,
+    to: recipients,
+    subject: "You are awesome!",
+    text: "Congrats for sending test email with Mailtrap!",
+    category: "Integration Test",
+  })
+  .then(console.log, console.error);
